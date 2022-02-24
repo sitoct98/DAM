c2dbb262816ac2f45687dab935791880b217270c
Sergio <Sergio@DESKTOP-M69M5BG>
2022-02-24 14:44:52 +0100

    primer paso

---
+++ src/dam_ed04_actividad/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+    
+        operativa_cuenta();
+    }
+
+    /**
+     *
+     */
+    public static void operativa_cuenta(){
+        float cantidad;
+        CCuenta Cuenta1;
+        double saldoActual;
+
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            Cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            Cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
